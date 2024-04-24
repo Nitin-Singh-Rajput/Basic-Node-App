8ea17aad28eae8ba07f55e7c206b167c0a6f3162
Rajputnk11 <nitinmyself11@gmail.com>
2024-04-25 00:32:32 +0530

    Basic Node App

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Node App');
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+app.get("/", (req, res) => {
+      res.send('Welcome to Basic Node App');
+})
+
+app.get("/about", (req, res) => {
+    res.send('This is a Basic Node App');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("server is running on port: "+PORT);
+})
